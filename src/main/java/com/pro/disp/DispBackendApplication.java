03bdb237269ed55d497393e9bb6fcc2499e187eb
Ruben Barrera <50993196+ifyum@users.noreply.github.com>
2022-10-22 06:59:04 -0300

    initial

---
+++ src/main/java/com/pro/disp/DispBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pro.disp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/disp/DispBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.disp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
