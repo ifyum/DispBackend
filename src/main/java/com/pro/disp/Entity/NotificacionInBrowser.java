bd4a95fd7f0b30af416993090ff6a6afd76214ac
Ruben Barrera <50993196+ifyum@users.noreply.github.com>
2023-01-03 00:00:36 -0300

    asdas

--- src/main/java/com/pro/disp/Entity/NotificacionInBrowser.java
+++ src/main/java/com/pro/disp/Entity/NotificacionInBrowser.java
@@ -20,7 +20,7 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "notificacion_in_browser")
-@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+//@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class NotificacionInBrowser implements Serializable {
 
     private static final long serialVersionUID = 1L;
