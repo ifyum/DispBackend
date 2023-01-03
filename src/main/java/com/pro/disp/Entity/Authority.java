f38ae01902ea72aed92e4b4cfe3e27dcda78dcae
Ruben Barrera <50993196+ifyum@users.noreply.github.com>
2023-01-02 23:42:24 -0300

    sada

--- src/main/java/com/pro/disp/Entity/Authority.java
+++ src/main/java/com/pro/disp/Entity/Authority.java
@@ -5,10 +5,7 @@ package com.pro.disp.Entity;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
