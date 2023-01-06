ae1e40ff94513e7246b6952ce0eacd4b8fc29bfd
Ruben Barrera <50993196+ifyum@users.noreply.github.com>
2023-01-06 04:02:19 -0300

    update

--- src/main/java/com/pro/disp/repository/AuthorityRepository.java
+++ src/main/java/com/pro/disp/repository/AuthorityRepository.java
@@ -1,13 +1,13 @@
-package com.pro.disp.repository;
-
-
-import com.pro.disp.Entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-/**
- * Spring Data JPA repository for the Authority entity.
- */
-@Repository
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
+//package com.pro.disp.repository;
+//
+//
+//import com.pro.disp.Entity.Authority;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+///**
+// * Spring Data JPA repository for the Authority entity.
+// */
+//@Repository
+//public interface AuthorityRepository extends JpaRepository<Authority, String> {
+//}
--- src/main/java/com/pro/disp/repository/MovimientoInvestigacionSumariaRepository.java
+++ src/main/java/com/pro/disp/repository/MovimientoInvestigacionSumariaRepository.java
@@ -1,17 +1,17 @@
-package com.pro.disp.repository;
-
-
-import com.pro.disp.Entity.MovimientoInvestigacionSumaria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-/**
- * Spring Data  repository for the MovimientoInvestigacionSumaria entity.
- */
-//@SuppressWarnings("unused")
-@Repository
-public interface MovimientoInvestigacionSumariaRepository extends JpaRepository<MovimientoInvestigacionSumaria, Long> {
-
-}
-
+//package com.pro.disp.repository;
+//
+//
+//import com.pro.disp.Entity.MovimientoInvestigacionSumaria;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//
+///**
+// * Spring Data  repository for the MovimientoInvestigacionSumaria entity.
+// */
+////@SuppressWarnings("unused")
+//@Repository
+//public interface MovimientoInvestigacionSumariaRepository extends JpaRepository<MovimientoInvestigacionSumaria, Long> {
+//
+//}
+//
