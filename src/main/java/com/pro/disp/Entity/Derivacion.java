6225ffb3c7e0ab82fe36be4fc957736defe01335
Ifyum <ifyumx@gmail.com>
2023-12-25 11:29:14 -0300

    se agrega DerivacionRepositoryTest

--- src/main/java/com/pro/disp/Entity/Derivacion.java
+++ src/main/java/com/pro/disp/Entity/Derivacion.java
@@ -28,8 +28,8 @@ public class Derivacion implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "derivacion_sequence")
+    @SequenceGenerator(name = "derivacion_sequence", sequenceName = "sequence_generator")
     private Long id;
 
     @Column(name = "observacion")
